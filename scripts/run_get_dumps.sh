17c94480e323c67304764e1c3049802c9c4c1c77
Marcilio Júnior <jrsmarcilio@gmail.com>
2026-04-29 09:15:21 -0300

    refactor: remover a escala bigcarga e descomentar o script CRUD

--- scripts/run_get_dumps.sh
+++ scripts/run_get_dumps.sh
@@ -3,7 +3,7 @@ set -e
 
 export $(grep -v '^#' ../.env | xargs)
 
-SCALES=("baixa" "media" "alta" "big")
+SCALES=("baixa" "media" "alta")
 
 for SCALE in "${SCALES[@]}"; do
     aws s3 cp s3://${BUCKET_NAME}/dumps/sysbench_prepare_$SCALE.tar.gz ../dumps/sysbench_prepare_$SCALE.tar.gz --profile tcc
